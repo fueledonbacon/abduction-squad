fd472e81e4cc0047cbd9926214b3279f68e653c7
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-04-13 15:52:43 -0500

    WIP Team Section

---
+++ components/basics/Member.vue
@@ -0,0 +1,47 @@
+<template>  
+	<div
+					class="
+						mx-auto
+						space-y-16
+						sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0
+						lg:grid-cols-3 lg:max-w-5xl
+					">
+  		<div
+         v-for="member in members"
+      :key="member.key"
+     class="space-y-6 text-center lg:ml-0 md:ml-10">
+						<div class="relative w-48 h-48 p-2 mx-auto md:w-56 md:h-56">
+							<img
+								class="relative z-20 w-full h-full rounded-full"
+								:src="member.pic"
+								alt="" />
+						</div>
+						<div class="space-y-2">
+							<div class="space-y-1 text-lg font-medium leading-6">
+								<h3 class="text-yellow">{{member.name}}</h3>
+								<p class="text-white">{{member.Role}}</p>
+							</div>
+							<div class="relative flex items-center justify-center space-x-3">
+								<a target="_newblank" :href="member.twitter" :v-if="member.twitter" class="text-gray-300 hover:text-gray-400">
+                  <font-awesome-icon :icon="['fab','twitter']" />
+								</a>
+
+								<a :href="member.in" v-if="member.in" class="text-gray-300 hover:text-gray-400">
+									                  <font-awesome-icon :icon="['fab','linkedin']" />
+								</a>
+							</div>
+						</div>
+					</div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    members: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
