3e2d3f27f457f4e8cf05124868e3e61ea0237028
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-04-15 17:00:42 -0500

    Fix members images and updates

--- components/basics/Timeline.vue
+++ components/basics/Timeline.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="grid  gap-5 gap-y-40 grid-flow-row md:grid-cols-4 grid-rows-2">
+	<div class="grid  gap-5 gap-y-32 grid-flow-row md:grid-cols-4 grid-rows-2">
 
     <div
 			class=" flex flex-col space-y-6 text-center lg:mx-4 md:ml-16 items-center"
@@ -9,8 +9,7 @@
 				style="font-family:'origflyingsausage'" class="w-2/12 sm:flex  text-yellow text-7xl">
 				{{ item.title }}
 			</h5>
-      <h3  style="font-family:'space-mono'" class="text-white whitespace-pre">
-        {{item.texts}}
+      <h3  style="font-family:'space-mono'" class="text-white whitespace-pre">{{item.texts}}
       </h3>
 
 		</div>
