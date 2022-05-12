435dcae56e242c5518aca29a226841d23fbdb629
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-05-12 11:10:14 -0500

    updating tests

--- test/03_minter.ts
+++ test/03_minter.ts
@@ -9,23 +9,48 @@ import {
 } from '../scripts/merkleTree'
 const payerAddress = '0xbcd4042de499d14e55001ccbb24a551f3b954096'
 const ipfs = 'ipfs://QmYfsUQEdmaaw8zDmR8qR7Cua6dakPGBuXsAPks7pedX4u/'
+const addresses = [
+	'0xcd46640FbE83642607234663e1de4728C9dBFeBF',
+	'0xC45F7C10bD2B6A171297dB2204Ad98b10b8a62a8',
+	'0x1ef402a94aA20B5EC99D7E9bF80C7ec51E5caC0B',
+	'0x89bF3AdD44784F157fc586BDaDACE514df8Ea452',
+]
 describe('ABDUCTION SQUAD:', function () {
+
 	it('Should be in whitelist', async function () {
-		const addresses = [
-			'0xcd46640FbE83642607234663e1de4728C9dBFeBF',
-			'0xC45F7C10bD2B6A171297dB2204Ad98b10b8a62a8',
-			'0x1ef402a94aA20B5EC99D7E9bF80C7ec51E5caC0B',
-			'0x89bF3AdD44784F157fc586BDaDACE514df8Ea452',
-		]
-		const merkleRoot = await createMerkleRoot(addresses)
-		const merkleProof = await createMerkleProof(addresses, addresses[2])
-
-		console.log( merkleRoot)
-
+		//Get Current msg.sender
+		const [_, minter] = await ethers.getSigners()
+		//add minter to whitelistaddresses
+		let addressesNew = [...addresses, minter.address]
+		// generate Merkle tree and get the root hash
+		const merkleRoot = await createMerkleRoot(addressesNew)
+		// generate merkle proor for minter address
+		const merkleProof = await createMerkleProof(addressesNew, minter.address)
+		//Deploy Smart Contract
 		const NFT = await ethers.getContractFactory('AbductionSquad')
 		const nft = await NFT.deploy(payerAddress, merkleRoot, merkleRoot, ipfs)
 		await nft.deployed()
-		const [_, minter] = await ethers.getSigners()
-		await nft.connect(minter).verifyWhitelist(merkleProof, addresses[2])
+		//Verify if minter address is in whitelist 
+		await nft.connect(minter).verifyWhitelist(merkleProof,minter.address)
+
+		
+	})
+
+	it('Should be avble to whiteList Mint', async function () {
+
+		const [deployer, user1, user2] = await ethers.getSigners();
+		let addressesNew  = [...addresses, user1.address]
+		const merkleRoot = await createMerkleRoot(addressesNew)
+		const merkleProof = await createMerkleProof(addressesNew, user1.address )
+		const NFT = await ethers.getContractFactory('AbductionSquad', {
+			signer: deployer,
+		})
+		const nft = await NFT.deploy(payerAddress, merkleRoot, merkleRoot, ipfs)
+		await nft.deployed()
+		await nft.connect(deployer).senderIsAdmin()
+		await nft.connect(deployer).toggleWhitelistSale()
+
+		await nft.connect(user1).whitelistMint(merkleProof,1)
+
 	})
 })
